ee3fef2887c9e1e21a0c0a14086ec9179d43d45a
Shayokh Shuvro <56217851+ShayokhShorfuddin@users.noreply.github.com>
2024-08-22 14:57:25 +0600

    Add files via upload

---
+++ components/ui/infinite-moving-cards.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import React, { useEffect, useState } from "react";
+
+export const InfiniteMovingCards = ({
+  items,
+  direction = "left",
+  speed = "fast",
+  pauseOnHover = true,
+  className,
+}: {
+  items: {
+    quote: string;
+    name: string;
+    title: string;
+    designation: string;
+  }[];
+  direction?: "left" | "right";
+  speed?: "fast" | "normal" | "slow";
+  pauseOnHover?: boolean;
+  className?: string;
+}) => {
+  const containerRef = React.useRef<HTMLDivElement>(null);
+  const scrollerRef = React.useRef<HTMLUListElement>(null);
+
+  useEffect(() => {
+    addAnimation();
+  }, []);
+  const [start, setStart] = useState(false);
+  function addAnimation() {
+    if (containerRef.current && scrollerRef.current) {
+      const scrollerContent = Array.from(scrollerRef.current.children);
+
+      scrollerContent.forEach((item) => {
+        const duplicatedItem = item.cloneNode(true);
+        if (scrollerRef.current) {
+          scrollerRef.current.appendChild(duplicatedItem);
+        }
+      });
+
+      getDirection();
+      getSpeed();
+      setStart(true);
+    }
+  }
+  const getDirection = () => {
+    if (containerRef.current) {
+      if (direction === "left") {
+        containerRef.current.style.setProperty(
+          "--animation-direction",
+          "forwards"
+        );
+      } else {
+        containerRef.current.style.setProperty(
+          "--animation-direction",
+          "reverse"
+        );
+      }
+    }
+  };
+  const getSpeed = () => {
+    if (containerRef.current) {
+      if (speed === "fast") {
+        containerRef.current.style.setProperty("--animation-duration", "20s");
+      } else if (speed === "normal") {
+        containerRef.current.style.setProperty("--animation-duration", "40s");
+      } else {
+        containerRef.current.style.setProperty("--animation-duration", "200s");
+      }
+    }
+  };
+  return (
+    <div
+      ref={containerRef}
+      className={cn("scroller max-w-full overflow-hidden", className)}
+    >
+      <ul
+        ref={scrollerRef}
+        className={cn(
+          "flex min-w-full shrink-0 gap-4 py-4 w-max flex-nowrap",
+          start && "animate-scroll ",
+          pauseOnHover && "hover:[animation-play-state:paused]"
+        )}
+      >
+        {items.map((item) => (
+          <li key={item.name}>
+            <div className="flex flex-col border border-gray-200 p-6 w-96 h-full rounded-lg hover:border-sedGreen transition duration-300 ease-in-out">
+              <h1 className="text-2xl">
+                {item.title}
+                <span className="text-sedGreen">.</span>
+              </h1>
+
+              <p className="mt-5 text-gray-600">{item.quote}</p>
+
+              <p className="mt-7">{item.name}</p>
+              <p className="mt-1 text-sm text-gray-600">{item.designation}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
