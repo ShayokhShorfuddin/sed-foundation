e297d34d74f601bfa9f067d4b0e6d3eedd4ba27f
Shayokh <shayokhshuvro14@gmail.com>
2026-01-27 22:29:03 +0600

    bring all queries from legacy codebase

---
+++ sanity/lib/queries.ts
@@ -0,0 +1,86 @@
+import { defineQuery } from 'next-sanity';
+
+export const EventsQuery = defineQuery(
+  `*[_type == "events"] | order(_createdAt desc){
+    _id,
+    _createdAt,
+    title,
+    "slug": slug.current,
+    content,
+    venue,
+    location_on_google_maps,
+    "cardImage": card_image{asset->{url}, alt},
+    from_date,
+    to_date
+  }`,
+);
+
+export const EventBySlugQuery = defineQuery(
+  `*[_type == "events" && slug.current == $slug][0]{
+    _id,
+    _createdAt,
+    title,
+    "slug": slug.current,
+    content,
+    venue,
+    location_on_google_maps,
+    "cardImage": card_image{asset->{url}, alt},
+    from_date,
+    to_date
+  }`,
+);
+
+export const IssuesQuery = defineQuery(
+  `*[_type == "issues"] | order(_createdAt desc){
+    _id,
+    _createdAt,
+    title,
+    "slug": slug.current,
+    coordinators,
+    "cardImage": card_image{asset->{url}, alt},
+  }`,
+);
+
+export const IssueBySlugQuery = defineQuery(
+  `*[_type == "issues" && slug.current == $slug][0]{
+    _id,
+    _createdAt,
+    title,
+    "slug": slug.current,
+    coordinators,
+    "cardImage": card_image{asset->{url}, alt},
+    "pdfUrl": pdf.asset->url,
+    
+    articles[] {
+      title,
+      authors,
+      keywords,
+      abstract,
+      "pdfUrl": pdf.asset->url
+    }
+  }`,
+);
+
+export const AllNewsQuery = defineQuery(
+  `*[_type == "news"] | order(_createdAt desc){
+    _id,
+    _createdAt,
+    title,
+    "slug": slug.current,
+    content,
+    "cardImage": card_image{asset->{url}, alt},
+    date
+  }`,
+);
+
+export const NewsBySlugQuery = defineQuery(
+  `*[_type == "news" && slug.current == $slug][0]{
+    _id,
+    _createdAt,
+    title,
+    "slug": slug.current,
+    content,
+    "cardImage": card_image{asset->{url}, alt},
+    date
+  }`,
+);
